1dc7c1da21b1eba7e7828dfd2ae23cd3f11fb147
Alec-Edstrom <90926217+Alec-Edstrom@users.noreply.github.com>
2021-09-17 12:12:01 -0500

    Add files via upload

---
+++ TheProjectFile.py
@@ -0,0 +1,14 @@
+def tempName():
+    print("Does nothing yet")
+
+
+def doesNothing():
+    print("Does nothing")
+
+
+
+def notInClass():
+    print("I was not in class today")
+
+
+##################Main Code##################
